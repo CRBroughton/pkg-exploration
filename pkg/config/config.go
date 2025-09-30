86bf068ac6da1dc92da486cbe30fe0aeaf46a2a9
CRBroughton <crbroughton@posteo.uk>
2025-09-30 19:26:43 +0100

    feat(config): :sparkles: add config package to load and save configs

---
+++ pkg/config/config.go
@@ -0,0 +1,36 @@
+package config
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/BurntSushi/toml"
+)
+
+type Config struct {
+	Name     string            `toml:"name"`
+	Packages map[string]string `toml:"packages"`
+}
+
+func LoadConfig(path string) (*Config, error) {
+	var cfg Config
+	if _, err := toml.DecodeFile(path, &cfg); err != nil {
+		return nil, fmt.Errorf("failed to parse config: %w", err)
+	}
+
+	if cfg.Name == "" {
+		return nil, fmt.Errorf("config.name is required")
+	}
+
+	return &cfg, nil
+}
+
+func (c *Config) Save(path string) error {
+	f, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	return toml.NewEncoder(f).Encode(c)
+}
