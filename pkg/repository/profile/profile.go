48b654b33066fd4092bfe373af7edd2d4509bbc5
CRBroughton <crbroughton@posteo.uk>
2025-09-30 18:49:55 +0100

    feat(profile): :sparkles: create ability to symlink files

---
+++ pkg/repository/profile/profile.go
@@ -0,0 +1,37 @@
+package profile
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+type Profile struct {
+	root string
+}
+
+func NewProfile(root string) *Profile {
+	return &Profile{
+		root: root,
+	}
+}
+
+func (p *Profile) Link(storePath string, binaries []string) error {
+	binDir := filepath.Join(p.root, "bin")
+	if err := os.MkdirAll(binDir, 0755); err != nil {
+		return err
+	}
+
+	for _, binary := range binaries {
+		source := filepath.Join(storePath, "bin", binary)
+		target := filepath.Join(binDir, binary)
+
+		os.Remove(target)
+
+		if err := os.Symlink(source, target); err != nil {
+			return fmt.Errorf("failed to link %s: %w", binary, err)
+		}
+	}
+
+	return nil
+}
